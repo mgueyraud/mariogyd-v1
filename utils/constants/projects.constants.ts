31d61ac6a774d03b3b74535bf2e61eb25235366e
mgueyraud <mgueyraud.junior@gmail.com>
2022-10-18 17:48:11 -0300

    Fixed some accessiblity issues

--- utils/constants/projects.constants.ts
+++ utils/constants/projects.constants.ts
@@ -11,6 +11,8 @@ export const projects = [
         description: "A block buster website (like Netflix) where it fetches movies from tmdb(The movie DB) so the user can see and select one movie to see more details about that movie.",
         cta: "https://github.com/mgueyraud/block-master/",
         imageSource: blockMaster,
+        altImage: 'Screenshot of the Block Master Project',
+        ariaLabelLink: 'See the source code of the Block Master Project'
     },
     {
         number: "02",
@@ -19,6 +21,8 @@ export const projects = [
         description: "A clone of the Spotify web page, where you can navigate into an album, and also hear music with my own library to play music in the DOM",
         cta: "https://github.com/mgueyraud/Spotifu-SpotifyClone",
         imageSource: spotifu,
+        altImage: 'Screenshot of the Spotifu Project',
+        ariaLabelLink: 'See the source code of the Spotifu Project'
     },
     {
         number: "03",
@@ -27,5 +31,7 @@ export const projects = [
         description: "A mobile app (Clone of the calculator of iOS), where you can do calculations built with React Native and Typescript",
         cta: "https://github.com/mgueyraud/calculator_iOS_RN",
         imageSource: calculator,
+        altImage: 'Screenshot of the Calculator App Project',
+        ariaLabelLink: 'See the source code of the Calculator App Project'
     },
 ];
